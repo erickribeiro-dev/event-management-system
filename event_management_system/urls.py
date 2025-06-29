bbc1f120606fbb71e3e8c6d03e10b2328b24176c
Erick <erickribeiro.dev@gmail.com>
2025-06-29 15:54:22 -0300

    Configure urls.py for events and accounts
    Configure urls.py for the project
    
    Add placeholder views for events and accounts

--- event_management_system/urls.py
+++ event_management_system/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), # Admin Dashboard
+    path("accounts/", include("accounts.urls")), # Accounts App
+    path("", include("events.urls")), # Events App
 ]
