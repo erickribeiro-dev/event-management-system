2638112948942ae95de721d548b0843c5833ff65
Erick <erickribeiro.dev@gmail.com>
2025-07-01 08:53:50 -0300

    Make the Event model prepopulate the slug field when entering Title in the admin panel, and display the event title and slug in the list of events

--- events/admin.py
+++ events/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Category, Tag, Event
 
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug",)
+    prepopulated_fields = {"slug": ("title",)}
+
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Event)
+# admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
