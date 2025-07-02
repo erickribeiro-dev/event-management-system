375d4721c6f1ab6d203f57196ce79c07afb28114
Erick <erickribeiro.dev@gmail.com>
2025-07-02 08:28:24 -0300

    Add custom user model for User, extending AbstractUser.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    """Represents an User account. This model extends AbstractUser."""
+    # Custom fields
+    bio = models.CharField(max_length=255, blank=True)
+
+    # Auditing
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
