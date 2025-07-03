cfc83baae43601242562ee8e50f4611abf0a1664
Erick <erickribeiro.dev@gmail.com>
2025-07-03 18:19:49 -0300

    Add delete_event view to urlpatterns.

--- events/urls.py
+++ events/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("events/<slug:slug>", views.EventDetailView.as_view(), name="event_detail"),
     path("events/create/", views.create_event, name="create_event"),
     path("events/update/<int:pk>", views.update_event, name="update_event"),
+    path("events/delete/<int:pk>/", views.delete_event, name="delete_event"),
 ]
