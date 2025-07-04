b515e00be8455d058aa4f2d0ffe850288bbd18d1
Erick <erickribeiro.dev@gmail.com>
2025-07-03 21:14:57 -0300

    Add search fields to Tag and Category in the admin dashboard

--- events/admin.py
+++ events/admin.py
@@ -2,13 +2,23 @@ from django.contrib import admin
 from .models import Category, Tag, Event
 
 # Register your models here.
+# ModelAdmin for Event
 class EventAdmin(admin.ModelAdmin):
     list_display = ("title", "created_at", "updated_at", "is_past", "is_ongoing", "is_upcoming")
     list_filter = ("category", "tags", "status")
     search_fields = ("title", "description", "category__name", "tags__name")
     prepopulated_fields = {"slug": ("title",)}
 
-admin.site.register(Category)
-admin.site.register(Tag)
-# admin.site.register(Event)
+# ModelAdmin for Category
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+# ModelAdmin for Tag
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
 admin.site.register(Event, EventAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
